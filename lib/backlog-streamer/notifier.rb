0d976298f6a281f5d4d0d25c8c2dcc20cad7092f
cynipe <cynipe@gmail.com>
2012-08-14 21:23:16 +0900

    Spikeで作った奴をとりあえず公開できる程度にした

---
+++ lib/backlog-streamer/notifier.rb
@@ -0,0 +1,22 @@
+require 'yammer'
+
+module Backlog
+  class Notifier
+
+    def initialize(config)
+      @yammer = Yammer.new(config)
+      raise ArgumentError, "Specified group does not exist: #{config[:group]}" unless group(config[:group])
+    end
+
+    def notify(timeline)
+      @yammer.update(timeline.to_s, :group_id => group.id)
+    end
+
+    def group(group = nil)
+      # 微妙にマッチしてない結果も返ってくるので再度名前で一致を取る
+      @group ||= @yammer.groups(:letter => group).find {|g| g.name == group}
+    end
+
+  end
+end
+
