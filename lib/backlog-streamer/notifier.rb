79a6cfb85ad571fb9038b56fd4a3da1e03368b20
cynipe <cynipe@gmail.com>
2012-08-17 21:39:01 +0900

    issueを辿って、登録者と担当者にメンションを飛ばすようにした
    イベントの作成者は除外する

--- lib/backlog-streamer/notifier.rb
+++ lib/backlog-streamer/notifier.rb
@@ -9,13 +9,25 @@ module Backlog
       raise ArgumentError, "Specified group does not exist: #{config[:group]}" unless group(config[:group])
     end
 
-    def notify(timeline)
-      @yammer.update(timeline.to_s, :group_id => group.id)
+    def notify(event, watchers)
+      @yammer.update(format(event, watchers), :group_id => group.id)
     end
 
+    private
     def group(group = nil)
       # 微妙にマッチしてない結果も返ってくるので再度名前で一致を取る
       @group ||= @yammer.groups(:letter => group).find {|g| g.name == group}
+    end
+
+    def format(event, watchers)
+      (<<-MSG).gsub(/^ +/, '')
+        #{event.user}によって"#{event.key}: #{event.summary}"が#{event.type}されました。
+        https://#{event.space}.backlog.jp/view/#{event.key}
+
+        #{event.content}
+
+        #{"cc: %s" % watchers.map {|w| "@#{w}" }.join(',') unless watchers.empty?}
+      MSG
     end
 
   end
