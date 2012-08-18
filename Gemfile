0701fffe871fd8366f8a343490143f2f4d012309
cynipe <cynipe@gmail.com>
2012-08-18 16:40:25 +0900

    backlog apiのテストを追加

--- Gemfile
+++ Gemfile
@@ -3,8 +3,12 @@ source :rubygems
 gem 'yammer'
 gem 'daemon-spawn'
 
+group :test do
+  gem 'rspec'
+  gem 'webmock'
+end
+
 group :development do
   gem 'pry'
-  gem 'rspec'
   gem 'reditor'
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,33 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'rspec'
+require 'pry'
+require 'webmock/rspec'
+require 'backlog-streamer'
+
+module FixtureHelpers
+  def timeline_response(name)
+    load_fixture("backlog/timeline/#{name}.xml")
+  end
+
+  def load_fixture(spec)
+    @@config_fixture_loaders ||= {}
+    @@config_fixture_loaders[spec] ||= File.read(File.dirname(__FILE__) + "/fixtures/#{spec}")
+    @@config_fixture_loaders[spec]
+  end
+
+end
+
+module BacklogApiHelpers
+  include FixtureHelpers
+
+  def prepare_timeline(fixture, status = 200)
+    stub_request(:post, "https://cynipe:xxxx@test.backlog.jp/XML-RPC").
+        with(:body => "<?xml version=\"1.0\" ?><methodCall><methodName>backlog.getTimeline</methodName><params/></methodCall>\n",
+             :headers => {'Accept'=>'*/*', 'Connection'=>'keep-alive', 'Content-Length'=>'101', 'Content-Type'=>'text/xml; charset=utf-8', 'User-Agent'=>'XMLRPC::Client (Ruby 1.9.3)'}).
+        to_return(:status => status,
+                  :body => timeline_response(fixture),
+                  :headers => {'Content-Type'=>'text/xml; charset=utf-8' })
+  end
+
+end
+
