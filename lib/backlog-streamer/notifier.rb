f9c478212251eb73cbc20fc27fada18b57b885fe
cynipe <cynipe@gmail.com>
2012-08-21 11:59:06 +0900

    スレッドに投稿する際は更新内容のみ投稿するようにし、種別が課題の場合は必ず新規で投稿するようにした

--- lib/backlog-streamer/notifier.rb
+++ lib/backlog-streamer/notifier.rb
@@ -7,27 +7,16 @@ module Backlog
   class Notifier
 
     attr_reader :yammer
+
     def initialize(config)
       @yammer = Yammer.new(config)
       raise ArgumentError, "Specified group does not exist: #{config[:group]}" unless group(config[:group])
     end
 
     def notify(event, watchers)
+      return update(event, watchers) if event.type == '課題'
       origin = find_origin(event)
-      if origin
-        @yammer.update(format(event, watchers), :group_id => group.id, :replied_to_id => origin.id)
-        puts "update found(threaded): #{u.type} #{u.summary}"
-      else
-        @yammer.update(format(event, watchers), :group_id => group.id)
-        puts "update found(new): #{u.type} #{u.summary}"
-      end
-    end
-
-    def find_origin(event)
-      res = @yammer.search("#{event.key}: #{event.summary}")
-      puts "#{event.key}: #{event.summary}"
-      return nil if res['count'].messages == 0
-      res.messages.messages.select {|m| m.group_id == group.id }.sort_by {|m| m.created_at }.first
+      origin.nil? ? update(event, watchers) : update_with_thread(event, watchers, origin)
     end
 
     private
@@ -36,8 +25,14 @@ module Backlog
       @group ||= @yammer.groups(:letter => group).find {|g| g.name == group}
     end
 
-    def format(event, watchers)
-      (<<-MSG).gsub(/^ +/, '')
+    def find_origin(event)
+      res = @yammer.search("#{event.key}: #{event.summary}")
+      return nil if res['count'].messages == 0
+      res.messages.messages.select {|m| m.group_id == group.id }.sort_by {|m| m.created_at }.first
+    end
+
+    def update(event, watchers)
+      @yammer.update((<<-MSG).gsub(/^ +/, ''), :group_id => group.id)
         #{event.user}によって"#{event.key}: #{event.summary}"が#{event.type}されました。
         https://#{event.space}.backlog.jp/view/#{event.key}
 
@@ -45,8 +40,19 @@ module Backlog
 
         #{"cc: %s" % watchers.map {|w| "@#{w}" }.join(',') unless watchers.empty?}
       MSG
+      puts "update found(new): #{event.type} #{event.summary}"
     end
 
+    def update_with_thread(event, watchers, origin)
+        @yammer.update((<<-MSG).gsub(/^ +/, ''), :group_id => group.id, :replied_to_id => origin.id)
+          #{event.user}によって#{event.type}されました。
+
+          #{event.content}
+
+          #{"cc: %s" % watchers.map {|w| "@#{w}" }.join(',') unless watchers.empty?}
+        MSG
+        puts "update found(threaded): #{event.type} #{event.summary}"
+    end
   end
 end
 
