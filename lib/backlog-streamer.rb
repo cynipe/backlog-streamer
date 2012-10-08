b1281306aedf87b46312a77c120e720717ea1aaf
cynipe <cynipe@gmail.com>
2012-10-08 13:17:15 +0900

    バージョンを0.0.4に

--- lib/backlog-streamer.rb
+++ lib/backlog-streamer.rb
@@ -8,7 +8,7 @@ module Backlog
 
   class Streamer < DaemonSpawn::Base
 
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
     STREAMER_ROOT = Pathname.new(File.expand_path(File.dirname(__FILE__) + '/..'))
 
     def initialize(args)
