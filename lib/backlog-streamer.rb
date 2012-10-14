faa2b68e487088cbf3fed042cb092c9265a9e728
cynipe <cynipe@gmail.com>
2012-10-14 17:27:50 +0900

    新しいconfig形式に対応させた

--- lib/backlog-streamer.rb
+++ lib/backlog-streamer.rb
@@ -26,8 +26,7 @@ module Backlog
         unless updates.empty?
           @last_updated = updates.last.updated_on
           updates.each do |u|
-            notifier.notify(u)
-            sleep 5
+            notifiers.each {|n| n.notify(u) }
           end
         end
         sleep 10
@@ -47,8 +46,8 @@ module Backlog
       @api ||= API.new(config[:backlog])
     end
 
-    def notifier
-      @notifier ||= Notifier.new(config[:yammer])
+    def notifiers
+      @notifiers ||= config[:notifiers].map {|config| Notifier.new(config) }
     end
   end
 end
